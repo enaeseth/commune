37187401e5f363f78477d2b9f04c5579e6431645
Eric Naeseth <enaeseth@gmail.com>
2009-10-07 19:56:07 -0500

    Adding source tracking and resource fetching facilities to ResourceServer.

--- commune/peer/server/ResourceServer.java
+++ commune/peer/server/ResourceServer.java
@@ -1,8 +1,64 @@
 package commune.peer.server;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * Serves requested resources asynchronously over pipe channels.
+ *
+ * A ResourceServer can serve files/resources from any number of
+ * {@link Source}s.
+ */
 public class ResourceServer {
+    private List<Source> sources;
+    private ReadWriteLock sourceLock;
+    private Lock sourceReadLock;
+    private Lock sourceWriteLock;
     
+    /**
+     * Creates a new resource server.
+     */
+    public ResourceServer() {
+        sources = new LinkedList<Source>();
+        sourceLock = new ReentrantReadWriteLock();
+        sourceReadLock = sourceLock.readLock();
+        sourceWriteLock = sourceLock.writeLock();
+    }
+    
+    /**
+     * Adds a new source to the server.
+     * @param source the source to add
+     */
+    public void addSource(Source source) {
+        sourceWriteLock.lock();
+        try {
+            sources.add(source);
+        } finally {
+            sourceWriteLock.unlock();
+        }
+    }
+    
+    /**
+     * Gets the resource available at the given path, if any.
+     * @param path the path of the desired resource
+     * @return the available resource, or null if no resource is available at
+     *         the given path
+     */
+    public AvailableResource getResource(String path) {
+        sourceReadLock.lock();
+        try {
+            AvailableResource resource;
+            for (Source source : sources) {
+                resource = source.getResource(path);
+                if (resource != null)
+                    return resource;
+            }
+            return null;
+        } finally {
+            sourceReadLock.unlock();
+        }
+    }
 }
